62d460a679dfd8c45f7b2e3f9f7dd233edd22bf2
lucasyoung <lucaswalkeryoung28@gmail.com>
2024-09-19 17:32:16 -0700

    Switched to 16-bit precision

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import torch.optim as optim
 import torch.nn as nn
 import torch
 
+from torch.cuda.amp import autocast
+from torch.cuda.amp import GradScaler
+
 from PIL import Image
 
 import os
@@ -37,6 +40,11 @@ transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
+reversals = transforms.Compose([
+    transforms.Normalize((-1, -1, -1), (2, 2, 2)),
+    transforms.ToPILImage()
+])
+
 
 # --------------------------------------------------------------------------------------------------
 # -------------------------------- Initialize the Models and Tools ---------------------------------
@@ -62,6 +70,7 @@ decoder = decoder.to(device)
 decoder.train(True)
 
 criterion = nn.MSELoss()
+scaler = GradScaler()
 
 dataset = datasets.ImageFolder(root='/content/drive/MyDrive/datasets/dataset', transform=transform)
 loader  = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)
@@ -83,32 +92,35 @@ if __name__ == '__main__':
             batch = batch.to(device)
             optimizer.zero_grad()
 
-            encoded = encoder(batch)
-            flattened = flattener(encoded)
-            resampled, mu, var = resampler(flattened)
-            projected = projector(resampled)
-            decoded = decoder(projected)
+            with autocast():
 
-            recon_loss = criterion(decoded, batch)
-            kl_loss = -0.5 * torch.sum(1 + var - mu.pow(2) - var.exp())  # KL Divergence
-            loss = recon_loss + kl_loss
-            running_loss += loss.item()
+                encoded = encoder(batch)
+                flattened = flattener(encoded)
+                resampled, mu, var = resampler(flattened)
+                projected = projector(resampled)
+                decoded = decoder(projected)
 
-            loss.backward()
-            optimizer.step()
+                recon_loss = criterion(decoded, batch)
+                kl_loss = -0.5 * torch.sum(1 + var - mu.pow(2) - var.exp())  # KL Divergence
+                loss = recon_loss + kl_loss
+                running_loss += loss.item()
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
 
             print(f'[{index + 1}] Loss: {loss.item():.4f}')
 
             for image in range(batch.size(0)):
 
-                source_image = reverse_transform(batch[i].cpu())
-                target_image = reverse_transform(decoded[i].cpu())
+                source_image = reversals(batch[image].cpu())
+                target_image = reversals(decoded[image].cpu())
                 merged_image = Image.new('RGB', (2048, 1024))
 
-                combined_image.paste(original_image, (0, 0))
-                combined_image.paste(recreated_image, (1024, 0))
+                combined_image.paste(source_image, (0, 0))
+                combined_image.paste(target_image, (1024, 0))
 
                 filename = f'{epoch:06}-{index:06}-{image:06}-{uuid.uuid4().replace("-", "")}.png'
-                combined_image.save(f'/content/drive/MyDrive/Output/{filename}')
+                merged_image.save(f'/content/drive/MyDrive/Output/{filename}')
 
         running_loss = 0
